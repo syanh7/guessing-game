04507ff9ed5b74d7759a47cf30fe1519ce86be34
Lisa Ma <lisamqy@gmail.com>
2021-07-13 22:59:13 +0000

    allows user to play multiple rounds

--- game.py
+++ game.py
@@ -4,35 +4,39 @@
 import random
 
 
+
+
+def game(name):
+    print("{}, I'm thinking of a number between 1 and 100".format(name))
+    print("Try to guess my number")
+
+    number = random.randint(1, 101)
+
+    guess = -1
+    tries = 0
+
+    while guess != number:
+        
+        try:
+            guess = int(input("Your guess?\n"))
+            if guess < number:
+                print("Your guess is too low, try again.")
+            elif guess > number:
+                print("Your guess is too high, try again.")
+            else: 
+                print(f"Well done, {name}! You found my number in {tries} tries!")
+        except:
+            print("Your guess is not a number, please try again.")
+            tries -= 1
+        tries += 1
+
+
+
+
 name = input("Howdy, what's your name?\n")
 
-print("{}, I'm thinking of a number between 1 and 100".format(name))
-print("Try to guess my number")
+flag = 'y'
 
-number = random.randint(1, 101)
-
-guess = -1
-tries = 0
-
-while guess != number:
-    
-    try:
-        guess = int(input("Your guess?\n"))
-        if guess < number:
-            print("Your guess is too low, try again.")
-        elif guess > number:
-            print("Your guess is too high, try again.")
-        else: 
-            print(f"Well done, {name}! You found my number in {tries} tries!")
-    except:
-        print("Your guess is not a number, please try again.")
-        tries -= 1
-    tries += 1
-
-
-
-
-
-
-
-
+while flag == 'y':
+    game(name)
+    flag = input("Would you like to play again? Enter 'y' for yes, enter anything for no").lower()
